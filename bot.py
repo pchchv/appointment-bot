c9f9f99dcab6cb2fbeb82cd3195b2ed4b90ad635
pchchv <ipchchv@gmail.com>
2022-07-28 18:21:49 +0000

    Implement a welcome function

--- bot.py
+++ bot.py
@@ -11,3 +11,13 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    eng_text = "Hi " + message.from_user.first_name  # TODO: Write welcoming messages
+    ru_text = "Привет " + message.from_user.first_name
+    await message.reply(eng_text)
