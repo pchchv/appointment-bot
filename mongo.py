3e96c00208ae959736d4a9266585c863d32218d3
pchchv <ipchchv@gmail.com>
2022-07-30 12:27:04 +0300

    Add a function finding a document in mongo

--- mongo.py
+++ mongo.py
@@ -20,3 +20,14 @@ def insert_document(collection, data):
     """ Function to insert a document into a collection and return the document's id.
     """
     return collection.insert_one(data).inserted_id
+
+
+def find_document(collection, elements, multiple=False):
+    """ Function to retrieve single or multiple documents from a provided
+    Collection using a dictionary containing a document's elements.
+    """
+    if multiple:
+        results = collection.find(elements)
+        return [r for r in results]
+    else:
+        return collection.find_one(elements)
