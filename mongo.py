a7f66f10ac9a821675bf086c9e17d779a25b5a0a
pchchv <ipchchv@gmail.com>
2022-07-30 12:22:46 +0300

    Add a mongo collections

--- mongo.py
+++ mongo.py
@@ -5,5 +5,12 @@ import pymongo
 def mongo_db():
     mongo_url = os.getenv('MONGO')
     client = pymongo.MongoClient(mongo_url)
-    db = client.test
+    db = client['appointments']
     print('Mongo connected')
+    collections(db)
+
+
+def collections(db):
+    specialist_collection = db['specialist']
+    user_collection = db['users']
+    appointment_collection = db['appointments']
