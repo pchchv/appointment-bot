772119d5cfbbbe599c5f7b08a2d2b03152990026
pchchv <ipchchv@gmail.com>
2022-07-30 12:25:47 +0300

    Add a function that inserts a document into a mongo

--- mongo.py
+++ mongo.py
@@ -14,3 +14,9 @@ def collections(db):
     specialist_collection = db['specialist']
     user_collection = db['users']
     appointment_collection = db['appointments']
+
+
+def insert_document(collection, data):
+    """ Function to insert a document into a collection and return the document's id.
+    """
+    return collection.insert_one(data).inserted_id
